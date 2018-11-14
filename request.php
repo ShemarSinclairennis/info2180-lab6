2b4a589ac310371a5ca8073193a0a593d28e521b
ShemarSinclairennis <shemar.ennis@gmail.com>
2018-11-13 22:29:47 -0500

    Exercise 4 comleted

--- request.php
+++ request.php
@@ -13,6 +13,60 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
+if ($query2 == 'true') {
+    $xmldata = '<?xml version="1.0" encoding="UTF-8"?>
 
+<entries>
+
+    <definition name ="definition" author = "Zack">
+
+        A statement of the exact meaning of a word, especially in a dictionary.
+
+    </definition>
+
+    <definition name ="bar" author = "Cody">
+
+        A place that sells alcholic beverages.
+
+    </definition>
+
+    <definition name ="ajax" author = "John">
+
+        Technique which involves the use of javascript and xml (or JSON).
+
+    </definition>
+
+    <definition name ="html" author = "Joe">
+
+       The standard markup language for creating web pages and web applications.
+
+    </definition>
+
+    <definition name ="css" author = "Mari">
+
+        A style sheet language used for describing the presentation of a document written in a markup language.
+
+    </definition>
+
+     <definition name ="javascript" author = "Shemar">
+
+        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+
+    </definition>
+
+     <definition name ="php" author = "Sage">
+
+        A server-side scripting language, and a powerful tool for making dynamic and interactive websites.
+
+    </definition>
+
+</entries>';
+
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput->asXML();
+    
+} else {
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
+}
